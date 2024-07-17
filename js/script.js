a865a3449f160192d39c3eb9bf49434b7b8de458
Youssef Mohammadi <youssefelmohammadi24@gmail.com>
2024-07-17 22:44:42 +0300

    fourth

--- js/script.js
+++ js/script.js
@@ -7,88 +7,77 @@ const typing = document.querySelector(".none"),
   succ = new Audio("/audio/YXFBY9J-win.mp3");
 
 const Words = [
-  { word: "react", disc: "JavaScript library" },
-  { word: "vue", disc: "JavaScript Framework" },
-  { word: "angular", disc: "JavaScript MVW Framework" },
-  { word: "nodejs", disc: "JavaScript runtime environment" },
-  { word: "php", disc: "general-purpose scripting language" },
-  { word: "ruby", disc: "open source programming language" },
-  { word: "python", disc: "Programming Language" },
-  { word: "tailwind", disc: "A utility-first CSS framework" },
-  { word: "bootstrap", disc: "world's most famous free CSS framework" },
+  // ... (words array)
 ];
 
-let Word,
-  countToWin = [],
-  maxGuess = 12;
+let Word;
+let countToWin = [];
+let maxGuess = 12;
 
 typing.addEventListener("input", startGame);
+getRandomWord();
 reset.addEventListener("click", getRandomWord);
 
 function getRandomWord() {
+  console.log("Getting a new random word.");
   freset();
-  const random = Math.floor(Math.random() * Words.length);
-  const randomObj = Words[random];
+  let random = Math.floor(Math.random() * Words.length);
+  let randomObj = Words[random];
   Word = randomObj.word;
   disc.textContent = randomObj.disc;
   guessCount.innerText = maxGuess;
-  inputsContainer.innerHTML = Word.split("")
-    .map(() => `<input type="text" disabled />`)
-    .join("");
-  typing.focus();
+  let inputs = "";
+  for (i = 0; i < Word.length; i++) {
+    inputs += `<input type="text" disabled />`;
+  }
+  inputsContainer.innerHTML = inputs;
+  console.log("Random word is:", Word);
 }
 
 document.addEventListener("keydown", () => typing.focus());
 
 function startGame(e) {
-  const char = e.target.value.toLowerCase();
+  char = e.target.value;
+  console.log("Input character:", char);
   if (!char.match(/[a-z]/i)) return;
-
-  let correctGuess = false;
   if (Word.includes(char)) {
-    Word.split("").forEach((letter, index) => {
+    for (i = 0; i < Word.length; i++) {
       if (
-        letter === char &&
-        !inputsContainer.querySelectorAll("input")[index].value
+        Word[i] === char &&
+        !inputsContainer.querySelectorAll("input")[i].value
       ) {
-        inputsContainer.querySelectorAll("input")[index].value = char;
+        inputsContainer.querySelectorAll("input")[i].value = char;
         countToWin.push(char);
-        correctGuess = true;
       }
-    });
-  }
-
-  if (!correctGuess) {
+    }
+  } else {
+    typing.value = "";
     maxGuess--;
     guessCount.innerText = maxGuess;
   }
 
   typing.value = "";
-
-  if (countToWin.length === Word.length) {
+  if (Word.length === countToWin.length) {
+    console.log("You win!");
     succ.play();
-    winner.classList.remove("hidden");
     countToWin = [];
+    winner.classList.remove("hidden");
   }
-
-  if (maxGuess <= 0) {
-    setTimeout(() => {
-      alert(`Game Over! The word was: ${Word}`);
-      Word.split("").forEach((letter, index) => {
-        inputsContainer.querySelectorAll("input")[index].value = letter;
-      });
-    }, 0);
-  }
+  setTimeout(() => {
+    if (maxGuess <= 0) {
+      alert("  😄 ههههههههههه حمقك  :)");
+      for (let i = 0; i < Word.length; i++) {
+        inputsContainer.querySelectorAll("input")[i].value = Word[i];
+      }
+    }
+  });
 }
 
 function freset() {
+  console.log("Resetting game.");
   maxGuess = 12;
-  guessCount.innerText = maxGuess;
   winner.classList.add("hidden");
   countToWin = [];
   succ.pause();
   succ.currentTime = 0;
-  typing.value = "";
 }
-
-getRandomWord();
